239aa0be96d5f9c8c78195d14248a3cf46057b8c
imran <87297004+imranshaikh9930@users.noreply.github.com>
2024-06-26 17:58:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 //your JS code here. If required.
 function throttle(callback, delay) {
 
+	let timeOut= null;
+	return (...args)=>{
+		if(!timeOut){
+			timeOut = setTimeout(()=>{
+				callback(...args)
+				timeOut = null;
+			},delay)
+		}
+	}
 }
 
 module.exports = throttle;
